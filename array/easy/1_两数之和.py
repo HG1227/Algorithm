576360b0e40baa1d8781db2f39dfb17c79e7aaed
hongghu <hongghu.@outlook.com>
2019-10-19 21:07:32 +0800

    dict() 存储

--- array/easy/1_两数之和.py
+++ array/easy/1_两数之和.py
@@ -5,4 +5,29 @@
 @software: PyCharm
 @file: 1_两数之和.py
 """
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_dict = dict()
+        for index, value in enumerate(nums):
+            sub = target - value
+            if sub in num_dict.keys():
+                return [num_dict[sub], index]
+            else:
+                num_dict[value] = index
+
+        return []
+
+if __name__=="__main__":
+    nums = [2, 7, 11, 15]
+    target = 9
+    c=Solution()
+    re=c.twoSum(nums,target)
+    print(re)
+
+
  
