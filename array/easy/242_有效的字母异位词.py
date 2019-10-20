58b6b4867ce581d0820a94d968e0ea20e51c2fab
hongghu <hongghu.@outlook.com>
2019-10-20 14:50:54 +0800

    str函数

--- array/easy/242_有效的字母异位词.py
+++ array/easy/242_有效的字母异位词.py
@@ -63,24 +63,24 @@ class Solution():
         :type t: str
         :rtype: bool
         """
-        if len(s)!=len(t):
+        if len(s) != len(t):
             return False
         else:
-            #结果字典
+            # 结果字典
             dic = {}
-            #先遍历s，数每个字符出现的次数，存在字典dic中，key是字符，value是出现次数
+            # 先遍历s，数每个字符出现的次数，存在字典dic中，key是字符，value是出现次数
             for i in s:
                 if i in dic:
-                    dic[i] +=1
+                    dic[i] += 1
                 else:
                     dic[i] = 1
-            #遍历t，遇见相同字符，就在dic中减一；遇见不同字符，直接返回false
+            # 遍历t，遇见相同字符，就在dic中减一；遇见不同字符，直接返回false
             for i in t:
                 if i in dic:
                     dic[i] -= 1
                 else:
                     return False
-            #遍历结果字典，是否每个value都为0
+            # 遍历结果字典，是否每个value都为0
             for i in dic:
                 if dic[i] != 0:
                     return False
@@ -91,7 +91,6 @@ if __name__ == "__main__":
     s = "anagram"
     t = "nagaram"
 
-
-    c=Solution()
-    re=c.isAnagram(s,t)
+    c = Solution()
+    re = c.isAnagram(s, t)
     print(re)
