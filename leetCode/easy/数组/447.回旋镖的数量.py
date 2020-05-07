c744be6b5f2f147114b45140327c3ff0a9a2ace6
hongghu <hongghu.@outlook.com>
2020-05-07 09:45:45 +0800

    回旋镖的数量

---
+++ leetCode/easy/数组/447.回旋镖的数量.py
@@ -0,0 +1,54 @@
+# -*- encoding: utf-8 -*-
+"""
+@software: PyCharm
+@file : 447.回旋镖的数量.py
+@time : 2020/5/7 
+"""
+"""
+给定平面上 n 对不同的点，“回旋镖” 是由点表示的元组 (i, j, k) ，其中 i 和 j 之间的距离和 i 和 k 之间的距离相等（需要考虑元组的顺序）。
+
+找到所有回旋镖的数量。你可以假设 n 最大为 500，所有点的坐标在闭区间 [-10000, 10000] 中。
+
+示例:
+
+输入:
+[[0,0],[1,0],[2,0]]
+
+输出:
+2
+
+解释:
+两个回旋镖为 [[1,0],[0,0],[2,0]] 和 [[1,0],[2,0],[0,0]]
+"""
+
+
+class Solution:
+    def numberOfBoomerangs(self, points: List[List[int]]) -> int:
+        res = 0
+        for p in points:
+            cmap = {}
+            for q in points:
+                dis = (p[0] - q[0]) ** 2 + (p[1] - q[1]) ** 2
+                cmap[dis] = cmap.get(dis, 0) + 1
+            for key in cmap:
+                res += (cmap[key]) * (cmap[key] - 1)
+        return res
+
+
+import collections
+
+
+class Solution2:
+    def numberOfBoomerangs(self, points: List[List[int]]) -> int:
+        ans = 0
+        for i in points:
+            dis = []
+            for j in points:
+                dis.append((j[1] - i[1]) ** 2 + (j[0] - i[0]) ** 2)
+            for j, k in collections.Counter(dis).items():
+                if j != 0 and k >= 2: ans += k * (k - 1)
+        return ans
+
+
+if __name__ == '__main__':
+    pass
