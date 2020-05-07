56a34a5f97b32024f9b0dd1ee34f5b4746784467
hongghu <hongghu.@outlook.com>
2020-05-07 08:54:21 +0800

    两个数组的交集

---
+++ leetCode/easy/数组/349.两个数组的交集.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+"""
+@software: PyCharm
+@file : 349.两个数组的交集.py
+@time : 2020/5/6 
+"""
+"""
+给定两个数组，编写一个函数来计算它们的交集。
+
+示例 1:
+
+输入: nums1 = [1,2,2,1], nums2 = [2,2]
+输出: [2]
+示例 2:
+
+输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+输出: [9,4]
+说明:
+
+输出结果中的每个元素一定是唯一的。
+我们可以不考虑输出结果的顺序。
+
+"""
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1=set(nums1)
+        nums2=set(nums2)
+        return list(nums1&nums2)
+if __name__ == '__main__':
+    pass
